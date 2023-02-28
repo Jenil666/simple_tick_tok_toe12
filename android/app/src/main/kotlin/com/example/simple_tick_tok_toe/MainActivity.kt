fea79583c9ac192df72f0dcd99d1219919feb1fe
Jenil666 <jenilchandarana33@gmail.com>
2023-02-28 10:47:23 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/simple_tick_tok_toe/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.simple_tick_tok_toe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Code extends StatefulWidget {
+  const Code({Key? key}) : super(key: key);
+
+  @override
+  State<Code> createState() => _CodeState();
+}
+
+class _CodeState extends State<Code> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text("Tick Tak Toe"),
+        ),
+        body: Column(
+          children: [],
+        ),
+      ),
+
+    );
+  }
+  Widget
+}
